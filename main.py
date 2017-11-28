619ff29415247f906624170793786b2d982d3c04
Ben Weller <ben.m.weller@gmail.com>
2017-11-27 20:56:43 -0600

    It works, now for creating a batch file and distributing it to friends.

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
+import re
 import sys
-
 import os
 from docx import Document
 from docx.shared import Pt, RGBColor
 from docx.enum.style import WD_STYLE_TYPE
+import pyperclip
+
 
 def change_to_dir():
-    if (len(sys.argv) < 3):
-        print("Please call the program in this format: programName pathToInputFile outputFileName")
+    if (len(sys.argv) < 3 or not pyperclip.paste()):
+        print("Please call the program in this format: programName pathToInputFile outputFileName\nAdditionally please have the text you would like added to the resume on your clipboard.")
     else:
-        # print(sys.argv[1])
-        # string = sys.argv[1].split("\\")
-        # input_file_name = string[-1]
-        # string.pop(-1)
-        # print(str(string))
-        # # os.chdir(sys.argv[1])
-        # return input_file_name
-        print(sys.argv[1])
         index_for_snipping = sys.argv[1].rfind("\\")
+        #I have to increment the index because it includes the \ and I need to get rid of that
         input_file_name = sys.argv[1][index_for_snipping+1:]
-        print(input_file_name)
-        # input_file_name = string[-1]
-        # string.pop(-1)
-        # print(str(string))
         os.chdir(sys.argv[1][:index_for_snipping])
-        print(os.getcwd())
         return input_file_name
 
-def insert_to_doc():
+def insert_to_doc(file_name):
+    document = Document(file_name)
 
-
-    document = Document('Resume.docx')
-
+    #Create a new style for me to use
     obj_styles = document.styles
     obj_charstyle = obj_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
     obj_font = obj_charstyle.font
@@ -39,9 +28,12 @@ def insert_to_doc():
     obj_font.name = 'Times New Roman'
     obj_font.color.rgb = RGBColor(255,255,255)
 
-    paragraph = document.add_paragraph().add_run('HELLO WORLD', style='CommentsStyle')
+    #String clensing, removing all white space that would just be "extra", trying to get everything on to one line
+    string = re.sub(r"\s{1,}", " ", pyperclip.paste())
 
-    document.save('WellerResume.docx')
+    document.add_paragraph().add_run(string, style='CommentsStyle')
+    document.save(sys.argv[2])
 
 if __name__ == "__main__":
-    change_to_dir()
+    file_name = change_to_dir()
+    insert_to_doc(file_name)
